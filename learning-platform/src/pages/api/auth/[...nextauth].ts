b209b717135ae07462282276e447e4fef357c68f
robertchinem <robertchinem@gmail.com>
2022-10-09 10:52:25 -0300

    chore: remove Discord auth provider

--- learning-platform/src/pages/api/auth/[...nextauth].ts
+++ learning-platform/src/pages/api/auth/[...nextauth].ts
@@ -1,25 +1,25 @@
-import NextAuth, { type NextAuthOptions } from "next-auth";
-import DiscordProvider from "next-auth/providers/discord";
-import { env } from "../../../env/server.mjs";
+import NextAuth, { type NextAuthOptions } from 'next-auth'
+// import DiscordProvider from 'next-auth/providers/discord'
+// import { env } from '../../../env/server.mjs'
 
 export const authOptions: NextAuthOptions = {
   // Include user.id on session
   callbacks: {
     session({ session, user }) {
       if (session.user) {
-        session.user.id = user.id;
+        session.user.id = user.id
       }
-      return session;
-    },
+      return session
+    }
   },
   // Configure one or more authentication providers
   providers: [
-    DiscordProvider({
-      clientId: env.DISCORD_CLIENT_ID,
-      clientSecret: env.DISCORD_CLIENT_SECRET,
-    }),
+    // DiscordProvider({
+    //   clientId: env.DISCORD_CLIENT_ID,
+    //   clientSecret: env.DISCORD_CLIENT_SECRET
+    // })
     // ...add more providers here
-  ],
-};
+  ]
+}
 
-export default NextAuth(authOptions);
+export default NextAuth(authOptions)
