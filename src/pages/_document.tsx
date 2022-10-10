c19a25ea369cc4aa73820f143cf0be03a23111f6
Renan Zago Lorijola <renanzago@gmail.com>
2022-10-10 14:45:49 +0000

    feat: grub title and favicon

---
+++ src/pages/_document.tsx
@@ -0,0 +1,20 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html lang="pt-BR">
+      <Head>
+          <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0"
+          />
+          <meta charSet="UTF-8"/>
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
