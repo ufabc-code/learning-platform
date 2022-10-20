a608133babf3d683491919f7b26140be81912d7f
robertchinem <robertchinem@gmail.com>
2022-10-20 12:09:26 -0300

    feat(ui components): add tab component and toas component

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,12 +4,9 @@ export default function Document() {
   return (
     <Html lang="pt-BR">
       <Head>
-          <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
-          <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1.0"
-          />
-          <meta charSet="UTF-8"/>
+        <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta charSet="UTF-8" />
       </Head>
       <body>
         <Main />
