d8faf6b2ad913bc754ce5c8c0606a959dde5ec86
robertchinem <robertchinem@gmail.com>
2022-10-20 00:28:57 -0300

    refactor(quiz editor): change input to textarea

--- src/pages/cursos/[courseId]/modules/components/quizLessonEditor.tsx
+++ src/pages/cursos/[courseId]/modules/components/quizLessonEditor.tsx
@@ -92,8 +92,7 @@ export function QuizLessonEditor({
             <input
               id="default-checkbox"
               type="checkbox"
-              value=""
-              className="h-4 w-4 rounded border-gray-300 bg-gray-100 text-blue-600 focus:ring-2 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:ring-offset-gray-800 dark:focus:ring-blue-600"
+              className="h-4 w-4 rounded border-gray-300 bg-gray-100 text-blue-600 focus:ring-2 focus:ring-blue-500"
               checked={quizLesson.solution.correct.includes(index)}
               onChange={(e) => {
                 if (e.target.checked) {
@@ -103,7 +102,7 @@ export function QuizLessonEditor({
                 }
               }}
             />
-            <input
+            <textarea
               value={alternative.text}
               onChange={(e) => {
                 alternative.text = e.target.value
