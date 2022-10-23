fd276329eda75feef5b760545ea933c126e810b3
Renan Zago Lorijola <renanzago@gmail.com>
2022-10-23 16:06:41 -0300

    feat: crud courses admin page
    Co-authored-by: AlineMilene <AlineMilene@users.noreply.github.com>

--- src/components/forms/switch/index.tsx
+++ src/components/forms/switch/index.tsx
@@ -1,22 +1,32 @@
-import { useState, FC } from 'react'
+import { useState, FC, InputHTMLAttributes } from 'react'
 
-type SwitchProps =  { onChangeChecked: (isChecked: boolean) => void}
+type SwitchProps = InputHTMLAttributes<HTMLInputElement> & {
+  onChangeChecked: (isChecked: boolean, id: string) => void
+}
 
-const Switch: FC<SwitchProps> = ({onChangeChecked}) => {
+const Switch: FC<SwitchProps> = ({ onChangeChecked, ...inputProps }) => {
   const [isChecked, setIsChecked] = useState(false)
   const handleIsChecked = () => {
     setIsChecked(!isChecked)
-    onChangeChecked(!isChecked)
+    inputProps.id && onChangeChecked(!isChecked, inputProps.id)
   }
-  console.log("COMPONENETE FILHO ",isChecked)
-  
+
   return (
-  <label htmlFor="default-toggle" className="inline-flex relative items-center cursor-pointer">
-    <input type="checkbox" value="" id="default-toggle" className="sr-only peer" onClick={handleIsChecked} checked={isChecked}/>
-    <div className="w-9 h-5 bg-gray-200 rounded-full peer peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-4 after:w-4 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
-    <span className="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300"></span>
-  </label>
+    <label
+      htmlFor={inputProps.id}
+      className="relative inline-flex cursor-pointer items-center"
+    >
+      <input
+        type="checkbox"
+        className="peer sr-only"
+        onChange={handleIsChecked}
+        checked={isChecked}
+        {...inputProps}
+      />
+      <div className="peer h-5 w-9 rounded-full bg-gray-200 after:absolute after:top-0.5 after:left-[2px] after:h-4 after:w-4 after:rounded-full after:border after:border-gray-300 after:bg-white after:transition-all after:content-[''] peer-checked:bg-blue-600 peer-checked:after:translate-x-full peer-checked:after:border-white peer-focus:ring-4 peer-focus:ring-blue-300 dark:border-gray-600 dark:bg-gray-700 dark:peer-focus:ring-blue-800"></div>
+      <span className="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300"></span>
+    </label>
   )
-} 
+}
 
 export default Switch
--- src/pages/admin/courses/index.tsx
+++ src/pages/admin/courses/index.tsx
@@ -3,50 +3,72 @@ import Switch from 'components/forms/switch'
 import IconButton from 'components/iconButton'
 import type { NextPage } from 'next'
 import { QueryClient } from 'react-query'
+import Course from 'server/entities/course'
 import { trpc, client } from 'utils/trpc'
 
 const Courses: NextPage = () => {
-  const courseQuery = trpc.useQuery(['courses.getAll'])
-  const createCourse = trpc.useMutation('courses.create')
-  function handleCreateCourse() {
-    createCourse.mutate(
+  const {
+    data: courses,
+    isError,
+    isLoading
+  } = trpc.useQuery(['courses.getAll'])
+  const { mutate: createCourse } = trpc.useMutation('courses.create')
+  const { mutate: deleteCourse } = trpc.useMutation('courses.delete')
+  const { mutate: updateCourse } = trpc.useMutation('courses.update')
+
+  const invalidateGetAll = () => client.invalidateQueries(['courses.getAll'])
+
+  const handleCreateCourse = () => {
+    createCourse(
       {
-        title: '',
+        title: 'title-' + courses?.length,
         description: '',
         modules: [],
         slug: ''
       },
       {
-        onSuccess: () => {
-          client.invalidateQueries(['courses.getAll'])
-        }
+        onSuccess: invalidateGetAll
       }
     )
   }
 
-  const valueSwitch = (isChecked: boolean) => {
-    console.log("COMPONENTE PAI ", isChecked)
-  } 
+  const handleSwitchChange = (isChecked: boolean, id: string) => {
+    // TODO send to api to activate/deactivate course
+  }
+
+  const handleDeleteCourse = (id: string) => {
+    deleteCourse({ id }, { onSuccess: invalidateGetAll })
+  }
+
+  const handleEditCourse = ({ id, ...course }: Course) => {
+    updateCourse({ course, id }, { onSuccess: invalidateGetAll })
+  }
 
   return (
     <AdminPageBase title="Gerenciar Cursos">
       <div className="flex flex-col">
-        <button className="mb-6 flex h-10 w-10 justify-center self-end rounded-full border-2 border-solid border-black align-middle text-3xl font-bold leading-none">
+        <button
+          onClick={handleCreateCourse}
+          className="mb-6 flex h-10 w-10 justify-center self-end rounded-full border-2 border-solid border-black align-middle text-3xl font-bold leading-none"
+        >
           +
         </button>
         <ul className="flex flex-col gap-y-2">
-          {[1,2,3,4,5].map((key) => (
-            <li key={key} className="flex border border-solid border-black">
-              <h6 className="flex-grow text-2xl">Lorem ipsum</h6>
-              <span className="flex-grow">Lorem ipsum</span>
-              <span className="flex-grow">Lorem, ipsum</span>
-              <div className="flex-grow flex items-center">
-                <IconButton onClick={() => {}}/>
-                <IconButton onClick={() => {}}/>
-                <Switch onChangeChecked={valueSwitch} />
-              </div>
-            </li>
-          ))}
+          {!!courses &&
+            courses.map(({ id, title, ...rest }) => (
+              <li key={id} className="flex border border-solid border-black">
+                <h6 className="w-1/4 text-2xl">{title}</h6>
+                <span className="w-1/4">Lorem ipsum</span>
+                <span className="w-1/4">Lorem, ipsum</span>
+                <div className="flex w-1/4 items-center">
+                  <IconButton onClick={() => handleDeleteCourse(id)} />
+                  <IconButton
+                    onClick={() => handleEditCourse({ id, title, ...rest })}
+                  />
+                  <Switch onChangeChecked={handleSwitchChange} id={id} />
+                </div>
+              </li>
+            ))}
         </ul>
       </div>
     </AdminPageBase>
