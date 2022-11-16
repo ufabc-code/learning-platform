eadf525c25618745b09e18f79f00d93a56be2531
AlineMilene <alinem2104@gmail.com>
2022-11-16 12:47:19 +0000

    feat: criando card para organização dos módulos

--- src/pages/student/courses/[courseId]/index.tsx
+++ src/pages/student/courses/[courseId]/index.tsx
@@ -1,3 +1,4 @@
+import MarkdownRender from 'components/markdownRender'
 import { useRouter } from 'next/router'
 import { trpc } from 'utils/trpc'
 
@@ -11,17 +12,27 @@ function Course() {
   if (!course) return null
 
   return (
-    <div>
-      <h1>modulos</h1>
-      {course.modules.map((module) => (
-        <a
-          href={`/student/courses/${courseId}/modules/${module.id}`}
-          key={course.id}
-        >
-          <h1>{module.id}</h1>
-        </a>
-      ))}
-    </div>
+    <>
+      <MarkdownRender content="# Módulos" />
+      <div className="max-w-sm rounded-lg border border-gray-200 bg-white p-6 shadow-md dark:border-gray-700 dark:bg-gray-800">
+        {course.modules.map((module) => (
+          <a
+            href={`/student/courses/${courseId}/modules/${module.id}`}
+            key={course.id}
+          >
+            <div className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+              <MarkdownRender content={module.id} />
+            </div>
+            <div>
+              <MarkdownRender content={module.description} />
+            </div>
+            <div>
+              <MarkdownRender content={module.title} />
+            </div>
+          </a>
+        ))}
+      </div>
+    </>
   )
 }
 
