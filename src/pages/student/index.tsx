48260898f73bd70568caca888caf52714288ee93
AlineMilene <alinem2104@gmail.com>
2022-11-23 12:42:40 +0000

    feat: estilização da página de estudante com a listagem do cursos

--- src/pages/student/index.tsx
+++ src/pages/student/index.tsx
@@ -1,3 +1,6 @@
+import MarkdownRender from 'components/markdownRender'
+import StudentPageBase from 'components/student/pageBase'
+import { courses } from 'server/router/courses/router'
 import { trpc } from 'utils/trpc'
 
 function Student() {
@@ -6,14 +9,31 @@ function Student() {
   console.log({ data: coursesQuery.data })
 
   return (
-    <div>
-      <h1>Student</h1>
-      {coursesQuery.data?.map((course) => (
-        <a href={`/student/courses/${course.id}`} key={course.id}>
-          {JSON.stringify(course.id, null, 2)}
-        </a>
-      ))}
-    </div>
+    <StudentPageBase title={'Cursos disponíveis'}>
+      <div className="flex flex-col">
+        <ul className="flex flex-col gap-y-2 text-gray-500 dark:text-gray-400">
+          {coursesQuery.data?.map((course) => (
+            <a href={`/student/courses/${course.id}`} key={course.id}>
+              <span className="flex items-center">
+                <svg
+                  className="mr-1.5 h-4 w-4 flex-shrink-0 text-green-500 dark:text-green-400"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
+                    clip-rule="evenodd"
+                  ></path>
+                </svg>
+                <h1>{course.title}</h1>
+              </span>
+            </a>
+          ))}
+        </ul>
+      </div>
+    </StudentPageBase>
   )
 }
 
