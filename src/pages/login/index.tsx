ebf8a7899a5393507a5ebbd898f10f08fbdc711b
Wesley <wesley.pereira@wildlifestudios.com>
2022-12-05 10:46:22 -0300

    fix: centralize login

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -7,7 +7,7 @@ export default function Login() {
   const [page, setPage] = useState<'login' | 'create-account'>('login')
 
   return (
-    <div className="flex h-screen items-center justify-center">
+    <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 items-center justify-center">
       {page === 'login' && (
         <SignIn email={email} setEmail={setEmail} setPage={setPage} />
       )}
